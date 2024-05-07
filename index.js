3c3941e61262e8db715536fc17a130c712725ae3
selvindenis97 <stambolicselvindenis@gmail.com>
2024-05-07 19:24:20 +0200

    Update index.js

--- index.js
+++ index.js
@@ -1,4 +1,4 @@
-const { Server } = require("socket.io");
+ni const { Server } = require("socket.io");
 
 require('dotenv').config();
 
@@ -34,4 +34,4 @@ io.on("connection", (socket) => {
     })
 });
 
-io.listen(3000);
+io.listen(process.env.PORT || 3000);
